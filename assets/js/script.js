1b2182e8f0ef7153c08570eb284474ccd777f6da
Agung Hidayatullah <162926832+agunghd@users.noreply.github.com>
2024-03-12 05:49:00 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         } else {
             document.querySelector('#scroll-top').classList.remove('active');
         }
-
+ 
         // scroll spy
         $('section').each(function () {
             let height = $(this).height();
